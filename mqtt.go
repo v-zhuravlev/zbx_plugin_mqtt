cf4e207db41586e7408534a21da32ca9d749e1ee
v-zhuravlev <zhuravlev.vitaly@gmail.com>
2019-12-11 14:59:34 +0300

    Remove passive item

--- mqtt.go
+++ mqtt.go
@@ -59,89 +59,22 @@ func (p *Plugin) Watch(requests []*plugin.Request, ctx plugin.ContextProvider) {
 			delete(impl.mqttClients, broker)
 		}
 	}
-
-	//reduce to key=mqtt.subscribe only
-	// subscribes := make([]*plugin.Request,0,1)
-	// for _,v := range requests {
-	// 	if v.Key == "mqtt.subscribe" {
-	// 		subscribes = append(subscribes, v)
-	// 	}
-	//}
-	
-	//append passive requests to active
-	for _,v := range p.passiveRequests {
-		requests = append(requests, v.request)
-	}
 	
 	p.manager.Update(ctx.ClientID(), ctx.Output(), requests)
 	p.manager.Unlock()
 	
-	//impl.Debugf("mqtt subscriptions(mqttSubs) we have after update %v\n", p.mqttSubs)
 	for _,v := range p.mqttSubs {
-		impl.Debugf("mqtt subs broker and topic: %s %s\n",v.broker, v.topic)
+		impl.Debugf("Registered MQTT subscriptions (broker and topic): %s %s\n",v.broker, v.topic)
 	}
-	impl.Debugf("mqtt mqttClients() we have after update %v\n", p.mqttClients)
+	impl.Debugf("Registered MQTT clients after update %v\n", p.mqttClients)
 
 }
 
-
-func (p *Plugin) Export(key string, params []string, ctx plugin.ContextProvider) (result interface{}, err error) {
-	if key == "mqtt.subscribe_passive" {
-		p.Debugf("export %s%v", key, params)	
-		p.manager.Lock()
-		defer p.manager.Unlock()
-		
-		es, err := p.EventSourceByKey(itemutil.MakeKey(key, params)); if err != nil {
-			return  nil, fmt.Errorf("Failed to create MQTT subscription(EventsourceByKey):%s", err)
-		}
-		mqttSub, ok := es.(*mqttSub); if !ok {
-			return  nil, fmt.Errorf("Failed to create MQTT subscription(not a mqttSub)")
-		}
-
-		fullKey := itemutil.MakeKey(key, params)
-		pRequest, ok := p.passiveRequests[fullKey]; if !ok {
-			request := &plugin.Request{
-				Itemid: ctx.ItemID(),
-				Key: fullKey,
-			}
-			pRequest = &passiveRequest{
-				request: request,
-			}
-			p.passiveRequests[fullKey] = pRequest
-		}
-		//updated modify time
-		pRequest.mtime = time.Now()
-
-		
-		// err = mqttSub.Subscribe(); if err != nil {
-		// 	return  nil, fmt.Errorf("Failed to create MQTT subscription(Subscribe):%s", err)
-		// }
-		
-
-		if (*mqttSub).last != nil {
-			impl.Debugf("Returning last value: %s\n",key)
-			filter := &itemFilter{}
-			return filter.Convert(*mqttSub.last)
-		}
-			
-		//break
-		//impl.Debugf("mqtt subscriptions(mqttSubs) we have after update %v\n", p.mqttSubs)
-		for _,v := range p.passiveRequests {
-			impl.Debugf("mqtt passibe subs: %s, modified %s\n",v.request.Key, v.mtime)
-		}
-		impl.Debugf("mqtt mqttClients() we have after passive update %v\n", p.mqttClients)
-
-	}
-	return &plugin.Result{}, nil
-}
-
-//our structure, would be mqtt struct
 type mqttSub struct {
-	broker  string // use it like key
+	broker  string
 	topic   string
 	manager *watch.Manager
 	connOpts MQTT.ClientOptions
-	last *MQTT.Message // last message. Passive subscribers can get it
 }
 
 
@@ -150,16 +83,6 @@ func (t *mqttSub) onMessageReceived(client MQTT.Client, message MQTT.Message) {
 	t.manager.Lock()
 	impl.Debugf("Received message on topic(filter): %s(%s), Message: %s\n", message.Topic(), t.topic, message.Payload())
 	t.manager.Notify(t, message)
-	t.last = &message
-
-	// for _,v := range impl.mqttZabbixTrappers {
-	// 	if v.topic == t.topic {
-	// 		impl.Debugf("Found Zabbix trapper subscription: %s for host %s", v.key, v.host)
-	// 		ZabbixSender(string(message.Payload()),v)
-	// 	}	
-	// }
-
-	
 	t.manager.Unlock()
 
 }
@@ -168,17 +91,11 @@ func (t *mqttSub) mqttConnect() (mqttClient *MQTT.Client, err error) {
 	impl.Infof("Checking for connection to %s\n", t.broker)
 	mqttClient, found := impl.mqttClients[t.broker]
 	if found {
-
 		if (*mqttClient).IsConnected() {
 			impl.Debugf("Already has connection\n")
-			//t.mqttClient = mqttClient
 		} else {
-			//delete, next subscription will try to reconnect
-			//delete(impl.mqttClients,t.broker)
 			return nil, MQTT.ErrNotConnected
-			
 		}
-
 	} else {
 		tmp := MQTT.NewClient(&t.connOpts)
 		mqttClient = &tmp
@@ -187,12 +104,10 @@ func (t *mqttSub) mqttConnect() (mqttClient *MQTT.Client, err error) {
 			impl.mqttClients[t.broker] = mqttClient
 			return nil, token.Error()
 		} else {
-
 			impl.Infof("Connected to %s\n", t.broker)
 			//add to list
 			impl.mqttClients[t.broker] = mqttClient
 		}
-
 	}
 
 	return mqttClient, nil
@@ -214,7 +129,6 @@ func (t *mqttSub) URI() (uri string) {
 	return t.broker + t.topic
 }
 
-// subscribe. create actual collector
 func (t *mqttSub) Subscribe() (err error) {
 
 	mqttClient, err := t.mqttConnect()
@@ -224,7 +138,6 @@ func (t *mqttSub) Subscribe() (err error) {
 	return t.mqttSubscribe(mqttClient)
 }
 
-//how to terminate
 func (t *mqttSub) Unsubscribe() {
 
 	mqttClient, err := t.mqttConnect()
@@ -258,8 +171,7 @@ func (f *itemFilter) Convert(v interface{}) (value *string, err error) {
 	return
 }
 
-//NewFilter parses item params. In MQTT We expect:
-//Server host:port, topic,
+
 func (t *mqttSub) NewFilter(key string) (filter watch.EventFilter, err error) {
 
 	return &itemFilter{}, nil
@@ -267,7 +179,7 @@ func (t *mqttSub) NewFilter(key string) (filter watch.EventFilter, err error) {
 }
 
 //EventSourceByURI is used to unsubscribe
-//from the sources without items associatede to them
+//from the sources without items associated to them
 func (p *Plugin) EventSourceByURI(uri string) (es watch.EventSource, err error) {
 
 	var ok bool
@@ -280,7 +192,6 @@ func (p *Plugin) EventSourceByURI(uri string) (es watch.EventSource, err error) 
 
 func (p *Plugin) newSub(broker string, topic string) (listener *mqttSub){
 
-	
 	clientid := p.options.ClientID
 	username := p.options.Username
 	password := p.options.Password
@@ -304,22 +215,17 @@ func (p *Plugin) newSub(broker string, topic string) (listener *mqttSub){
 
 	return listener
 	
-
 }
 
 //EventSourceByKey is used when trying to match item key
-// with it's event source during item update
+//with it's event source during item update
 func (p *Plugin) EventSourceByKey(key string) (es watch.EventSource, err error) {
 	var params []string
 	if _, params, err = itemutil.ParseKey(key); err != nil {
 		return
 	}
-	
 
-	//mqtt broker here
-	//The full url of the MQTT broker to connect to ex: tcp://127.0.0.1:1883")
 	var broker string
-
 	if len(params) == 0 {
 		return
 	}
@@ -333,15 +239,6 @@ func (p *Plugin) EventSourceByKey(key string) (es watch.EventSource, err error) 
 		topic = "#"
 	}
 
-	// var qos int
-	// if len(params) > 2 {
-	// 	qos, err = strconv.Atoi(params[2]); if err !=nil {
-	// 		return
-	
-	// 	}
-	// } else {
-	// 	qos = 0
-	// }
 
 	var ok bool
 	var listener *mqttSub
@@ -361,12 +258,8 @@ func (p *Plugin) EventSourceByKey(key string) (es watch.EventSource, err error) 
 
 
 func init() {
-
 	impl.manager = watch.NewManager(&impl)
 	impl.mqttSubs = make(map[string]*mqttSub)
 	impl.mqttClients = make(map[string]*MQTT.Client)
-	impl.passiveRequests = make(map[string]*passiveRequest)
 	plugin.RegisterMetrics(&impl, "MQTTSubscribe", "mqtt.subscribe", "Subscribe to MQTT topic and receive messages when they arrive.")
-	plugin.RegisterMetrics(&impl, "MQTTSubscribe", "mqtt.subscribe_passive", "Subscribe to MQTT topic using Zabbix agent in passive mode.")
-
 }
