4f8215f01da786ad6b69e6a2678cfa4b90007c6d
v-zhuravlev <zhuravlev.vitaly@gmail.com>
2019-12-11 15:01:07 +0300

    Apply format

--- mqtt.go
+++ mqtt.go
@@ -20,12 +20,12 @@
 package mqtt
 
 import (
+	"crypto/tls"
 	"fmt"
+	"time"
 	"zabbix.com/pkg/itemutil"
 	"zabbix.com/pkg/plugin"
 	"zabbix.com/pkg/watch"
-	"crypto/tls"
-	"time"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
@@ -33,25 +33,25 @@ import (
 // Plugin
 type Plugin struct {
 	plugin.Base
-	manager     *watch.Manager
-	mqttSubs   map[string]*mqttSub
-	mqttClients map[string]*MQTT.Client
-	passiveRequests   map[string]*passiveRequest
-	options Options
+	manager         *watch.Manager
+	mqttSubs        map[string]*mqttSub
+	mqttClients     map[string]*MQTT.Client
+	passiveRequests map[string]*passiveRequest
+	options         Options
 }
 
 var impl Plugin
 
 type passiveRequest struct {
 	request *plugin.Request
-	mtime time.Time
-} 
+	mtime   time.Time
+}
 
 //Actual plugin
 func (p *Plugin) Watch(requests []*plugin.Request, ctx plugin.ContextProvider) {
-	
+
 	p.manager.Lock()
-	
+
 	//clear broken or invalid connections on agent sync
 	for broker, mqttClient := range impl.mqttClients {
 		if !(*mqttClient).IsConnected() {
@@ -59,27 +59,26 @@ func (p *Plugin) Watch(requests []*plugin.Request, ctx plugin.ContextProvider) {
 			delete(impl.mqttClients, broker)
 		}
 	}
-	
+
 	p.manager.Update(ctx.ClientID(), ctx.Output(), requests)
 	p.manager.Unlock()
-	
-	for _,v := range p.mqttSubs {
-		impl.Debugf("Registered MQTT subscriptions (broker and topic): %s %s\n",v.broker, v.topic)
+
+	for _, v := range p.mqttSubs {
+		impl.Debugf("Registered MQTT subscriptions (broker and topic): %s %s\n", v.broker, v.topic)
 	}
 	impl.Debugf("Registered MQTT clients after update %v\n", p.mqttClients)
 
 }
 
 type mqttSub struct {
-	broker  string
-	topic   string
-	manager *watch.Manager
+	broker   string
+	topic    string
+	manager  *watch.Manager
 	connOpts MQTT.ClientOptions
 }
 
-
 func (t *mqttSub) onMessageReceived(client MQTT.Client, message MQTT.Message) {
-	
+
 	t.manager.Lock()
 	impl.Debugf("Received message on topic(filter): %s(%s), Message: %s\n", message.Topic(), t.topic, message.Payload())
 	t.manager.Notify(t, message)
@@ -149,15 +148,13 @@ func (t *mqttSub) Unsubscribe() {
 		}
 	}
 	delete(impl.mqttSubs, t.URI())
-	
+
 	//TODO
 	//delete mqttClient
 }
 
 type itemFilter struct {
-
 }
-
 
 //Convert and filter
 func (f *itemFilter) Convert(v interface{}) (value *string, err error) {
@@ -165,12 +162,11 @@ func (f *itemFilter) Convert(v interface{}) (value *string, err error) {
 	if b, ok := v.(MQTT.Message); !ok {
 		err = fmt.Errorf("unexpected traper conversion input type %T", v)
 	} else {
-			tmp := string(b.Payload())
-			value = &tmp
+		tmp := string(b.Payload())
+		value = &tmp
 	}
 	return
 }
-
 
 func (t *mqttSub) NewFilter(key string) (filter watch.EventFilter, err error) {
 
@@ -189,8 +185,7 @@ func (p *Plugin) EventSourceByURI(uri string) (es watch.EventSource, err error) 
 	return
 }
 
-
-func (p *Plugin) newSub(broker string, topic string) (listener *mqttSub){
+func (p *Plugin) newSub(broker string, topic string) (listener *mqttSub) {
 
 	clientid := p.options.ClientID
 	username := p.options.Username
@@ -210,11 +205,11 @@ func (p *Plugin) newSub(broker string, topic string) (listener *mqttSub){
 		broker:   broker,
 		manager:  p.manager,
 		connOpts: *connOpts, //TODO move it somewhere
-		topic: topic,
+		topic:    topic,
 	}
 
 	return listener
-	
+
 }
 
 //EventSourceByKey is used when trying to match item key
@@ -239,23 +234,18 @@ func (p *Plugin) EventSourceByKey(key string) (es watch.EventSource, err error) 
 		topic = "#"
 	}
 
-
 	var ok bool
 	var listener *mqttSub
 
 	//if not exist, create?
 	if listener, ok = p.mqttSubs[broker+topic]; !ok {
 		impl.Debugf("MQTT Subscription %s not found, going to prepare one for broker %s\n", topic, broker)
-		listener = p.newSub(broker,topic)
+		listener = p.newSub(broker, topic)
 		p.mqttSubs[broker+topic] = listener
 	}
 
 	return listener, nil
 }
-
-
-
-
 
 func init() {
 	impl.manager = watch.NewManager(&impl)
--- mqtt_configure.go
+++ mqtt_configure.go
@@ -1,14 +1,11 @@
 package mqtt
 
-
 import (
-
+	"os"
+	"strconv"
+	"time"
 	"zabbix.com/pkg/conf"
 	"zabbix.com/pkg/plugin"
-	"strconv"
-	"os"
-	"time"
-
 	// MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
@@ -19,28 +16,25 @@ type Options struct {
 	ClientID string // MQTT clientid to use
 }
 
-
 func (p *Plugin) Configure(global *plugin.GlobalOptions, private interface{}) {
 
 	//p.options.Interval = 10
 	p.options.Username = ""
 	p.options.Password = ""
-	
-	
+
 	hostname, _ := os.Hostname()
 	p.options.ClientID = hostname + strconv.Itoa(time.Now().Second())
 
 	if err := conf.Unmarshal(private, &p.options); err != nil {
 		p.Warningf("cannot unmarshal configuration options: %s", err)
 	}
-	
+
 	//p.Debugf("configure: interval=%d", p.options.Interval)
 	p.Debugf("configure: username=%s", p.options.Username)
 	p.Debugf("configure: clientid=%s", p.options.ClientID)
 	//p.Debugf("configure: password is set")
 
 }
-
 
 func (p *Plugin) Validate(private interface{}) (err error) {
 	return
